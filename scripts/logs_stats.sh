3937da59c1753686ba07da7f32b663eeae9abc42
alex <alexxpyykonen@gmail.com>
2024-05-09 05:45:16 +0300

    Modifie nginx to only expose stremio-web gui and the jellyifn to the internet

--- scripts/logs_stats.sh
+++ scripts/logs_stats.sh
@@ -12,11 +12,17 @@ tmux send-keys -t docker_monitor:0.0 "docker compose logs --follow --timestamps 
 # In the right pane (pane 1), split vertically into two panes
 tmux split-window -v
 
-# In the top right pane (pane 1.0), show Docker stats
-tmux send-keys -t docker_monitor:0.1 "docker stats" C-m
+# In the top right pane (pane 1.0), split it again vertically
+tmux split-window -v -t docker_monitor:1.0
 
-# In the bottom right pane (pane 1.1), monitor Docker sockets with lsof
-tmux send-keys -t docker_monitor:0.2 "while true; do sudo lsof -i -P -n | grep docker; sleep 10; clear; done" C-m
+# In the top right pane (pane 1.0), now show Docker stats
+tmux send-keys -t docker_monitor:1.0 "docker stats" C-m
+
+# In the middle right pane (pane 1.1), monitor Docker sockets with lsof
+tmux send-keys -t docker_monitor:1.1 "while true; do sudo lsof -i -P -n | grep docker; sleep 10; clear; done" C-m
+
+# In the bottom right pane (pane 1.2), display systemctl status for Docker
+tmux send-keys -t docker_monitor:1.2 "watch systemctl status docker" C-m
 
 # Attach to the tmux session
 tmux attach-session -t docker_monitor
