dd2993ab0ec9c712cdd053370e86086b0b093182
alex <alexxpyykonen@gmail.com>
2024-05-13 06:07:21 +0300

    + jelly works trough nginx but stremio not yet + Simplify the nginx configuration + ADd new profile for all containers "all"

--- scripts/dockerlog.sh
+++ scripts/dockerlog.sh
@@ -2,3 +2,4 @@
 docker compose logs --follow --timestamps | ccze -A
 docker compose --profile streaming logs --follow --timestamps | ccze -A
 docker compose --profile install logs --follow --timestamps| ccze -A
+docker compose --profile all logs --follow --timestamps | ccze -A
